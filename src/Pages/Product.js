74c957bf17aacaadc6a1213774ceaf794f737a8c
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-01-28 15:19:09 +0530

    fixes

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -3,6 +3,18 @@ import React from "react";
 import Carousel from "react-elastic-carousel";
 import Item from "../Components/Item";
 import "../css/style.css";
+import image1 from '../Assests/1.jpeg'
+import image2 from '../Assests/2.jpeg'
+import image3 from '../Assests/3.jpeg'
+import image4 from '../Assests/4.jpeg'
+import image5 from '../Assests/5.jpeg'
+import image6 from '../Assests/6.jpeg'
+import image7 from '../Assests/7.jpeg'
+import image8 from '../Assests/8.jpeg'
+import image9 from '../Assests/9.jpeg'
+import image10 from '../Assests/10.jpeg'
+import image11 from '../Assests/11.jpeg'
+import image12 from '../Assests/12.jpeg'
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
@@ -24,23 +36,23 @@ function Product() {
 				</div>
 			</div>
         <Carousel breakPoints={breakPoints}>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/1_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/2_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/3_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/4_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/5_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/6_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/7_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/9_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/8_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/10_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/11_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/12_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/13_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/14_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/15_223x226.jpeg"/> </Item>
+          <Item><img src={image1}/> </Item>
+          <Item><img src={image2}/> </Item>
+          <Item><img src={image3}/> </Item>
+          <Item><img src={image4}/> </Item>
+          <Item><img src={image5}/> </Item>
+          <Item><img src={image6}/> </Item>
+          <Item><img src={image7}/> </Item>
+          <Item><img src={image8}/> </Item>
+          <Item><img src={image8}/> </Item>
+          <Item><img src={image8}/> </Item>
+          <Item><img src={image8}/> </Item>
+          <Item><img src={image8}/> </Item>
+          <Item><img src={image8}/> </Item>
+          <Item><img src={image8}/> </Item>
+          <Item><img src={image8}/> </Item>
           <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/16_223x226.jpeg"/> </Item>
-          <Item><img src="http://www.simpleimageresizer.com/_uploads/photos/aef928fe/17_223x226.jpeg"/> </Item>
+          <Item><img src="https://bit.ly/34cNNTp"/> </Item>
 
         </Carousel>
       </div>
