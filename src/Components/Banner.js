768235193511c5241b9673941f31294ae6766134
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-02-02 20:46:56 +0530

    fixes

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -9,11 +9,11 @@ function Banner() {
 		<div className='banner123'>
 			<img  src={banner12} alt='img' />
 			<h3 className='heading'>Welcome to </h3>
-			<h3 className='heading123'>Zenberg Pharmaseuticals </h3>
+			<h3 className='heading123'>Zenberg <span className='text-orange-500'>Pharmaceuticals</span> </h3>
 
 			<p className='para'>India’s leading pharmaceutical company</p>
 			
-		<a href='#about'>	<button  className='btn1'>Learn More</button></a>
+		
 		</div>
 	</div>
 
