4c322616dd780d127e6e4201836f30b452910d94
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-02-14 22:55:03 +0530

    changes the banner image

--- src/Components/Address.js
+++ src/Components/Address.js
@@ -4,43 +4,43 @@ import '../css/custom.css'
 import '../css/pogo-slider.min.css'
 import '../css/bootstrap.min.css'
 
-function Address(){
-    return (
-        <div class="col-lg-12 col-xs-12">
-					<div class="left-contact">
-						<h2>Address</h2>
-						<div class="media cont-line">
-							<div class="media-left icon-b">
-								<i class="fa fa-location-arrow" aria-hidden="true"></i>
-							</div>
-							<div class="media-body dit-right">
-								<h4>Address</h4>
-								<p>Fleming 196 Woodside Circle Mobile, FL 36602</p>
-							</div>
-						</div>
-						<div class="media cont-line">
-							<div class="media-left icon-b">
-								<i class="fa fa-envelope" aria-hidden="true"></i>
-							</div>
-							<div class="media-body dit-right">
-								<h4>Email</h4>
-								<a href="#">demoinfo@gmail.com</a><br/>
-								<a href="#">demoinfo@gmail.com</a>
-							</div>
-						</div>
-						<div class="media cont-line">
-							<div class="media-left icon-b">
-								<i class="fa fa-volume-control-phone" aria-hidden="true"></i>
-							</div>
-							<div class="media-body dit-right">
-								<h4>Phone Number</h4>
-								<a href="#">12345 67890</a><br/>
-								<a href="#">12345 67890</a>
-							</div>
-						</div>
+function Address() {
+	return (
+		<div class="col-lg-12 col-xs-12">
+			<div class="left-contact">
+				<h2>Address</h2>
+				<div class="media cont-line">
+					<div class="media-left icon-b">
+						<i class="fa fa-location-arrow" aria-hidden="true"></i>
+					</div>
+					<div class="media-body dit-right">
+						<h4>Address</h4>
+						<p>Fleming 196 Woodside Circle Mobile, FL 36602</p>
 					</div>
 				</div>
-    )
+				<div class="media cont-line">
+					<div class="media-left icon-b">
+						<i class="fa fa-envelope" aria-hidden="true"></i>
+					</div>
+					<div class="media-body dit-right">
+						<h4>Email</h4>
+						<a href="#">demoinfo@gmail.com</a><br />
+						<a href="#">demoinfo@gmail.com</a>
+					</div>
+				</div>
+				<div class="media cont-line">
+					<div class="media-left icon-b">
+						<i class="fa fa-volume-control-phone" aria-hidden="true"></i>
+					</div>
+					<div class="media-body dit-right">
+						<h4>Phone Number</h4>
+						<a href="#">12345 67890</a><br />
+						<a href="#">12345 67890</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Address;
--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,6 +1,6 @@
 import '../css/style.css'
 import React, { useState } from 'react';
-import banner12 from '../Assests/banner.jpeg'
+import banner12 from '../Assests/banner.jpg'
 function Banner() {
 
 	return (
@@ -9,7 +9,7 @@ function Banner() {
 		<div className='banner123'>
 			<img  src={banner12} alt='img' />
 			<h3 className='heading'>Welcome to </h3>
-			<h3 className='heading123'>Zenberg <span className='text-orange-500'>Pharmaceutical</span> </h3>
+			<h3 className='heading123'>Zenberg <span className='text-white'>Pharmaceutical</span> </h3>
 
 			<p className='para'>India’s leading pharmaceutical company</p>
 			
