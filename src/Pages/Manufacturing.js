3af0c342fcee29660b12d7543bc103c27cdbfd08
suyalamritanshu <amritanshu.suyall2@gmail.com>
2022-04-20 20:15:10 +0530

    added all the logos

--- src/Pages/Manufacturing.js
+++ src/Pages/Manufacturing.js
@@ -3,24 +3,13 @@ import React from "react";
 import Carousel from "react-elastic-carousel";
 import Item from "../Components/Item";
 import "../css/style.css";
-import image1 from '../Assests/1.svg'
-import image2 from '../Assests/2.svg'
-import image3 from '../Assests/3.svg'
-import image4 from '../Assests/4.svg'
-import image5 from '../Assests/5.svg'
-import image6 from '../Assests/10.svg'
-import image7 from '../Assests/7.svg'
-import image8 from '../Assests/8.svg'
-import image9 from '../Assests/9.svg'
-import image10 from '../Assests/10.svg'
-import image11 from '../Assests/11.svg'
-import image12 from '../Assests/12.svg'
-import image13 from '../Assests/13.svg'
-import image14 from '../Assests/14.svg'
-import image15 from '../Assests/15.svg'
-import image16 from '../Assests/16.svg'
-import image17 from '../Assests/17.svg'
-import {useState, useEffect} from 'react';
+import image1 from '../Assests/logo10.png'
+import image2 from '../Assests/unnamed.jpg'
+import image3 from '../Assests/download.jpg'
+import image4 from '../Assests/download1.png'
+import image5 from '../Assests/download1.png'
+import image6 from '../Assests/download1.png'
+
 
 
 const breakPoints = [
@@ -49,18 +38,9 @@ function Manufacturing() {
         <Item><a href='/product-3'><img src={image3}/></a> </Item>
         <Item><a href='/product-4'><img src={image4}/></a> </Item>
         <Item><a href='/product-5'><img src={image5}/></a> </Item>
-        <Item><a href='/product-6'><img src={image6}/></a> </Item>
-        <Item><a href='/product-7'><img src={image7}/></a> </Item>
-        <Item><a href='/product-8'><img src={image8}/></a> </Item>
-        <Item><a href='/product-9'><img src={image9}/></a> </Item>
-        <Item><a href='/product-10'><img src={image10}/></a> </Item>
-        <Item><a href='/product-11'><img src={image11}/></a> </Item>
-        <Item><a href='/product-12'><img src={image12}/></a> </Item>
-        <Item><a href='/product-13'><img src={image13}/></a> </Item>
-        <Item><a href='/product-14'><img src={image14}/></a> </Item>
-        <Item><a href='/product-15'><img src={image15}/></a> </Item>
-        <Item><a href='/product-16'><img src={image16}/></a> </Item>
-        <Item><a href='/product-17'><img src={image17}/></a> </Item>
+        <Item><a href='/product-5'><img src={image6}/></a> </Item>
+
+
 
 
 
